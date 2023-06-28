dca1796a3626ef4bcbd1548a894a4e6901ccc946
Pavel Krivtsov <krivcov.pavel@gmail.com>
2023-06-28 19:29:48 +0300

    Initial Commit

---
+++ HS_TZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HS_TZ
+//
+//  Created by Pavel Krivtsov on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
